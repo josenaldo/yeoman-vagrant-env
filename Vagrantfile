68408c9981c3c84415aa45812f208a2bc74be3af
josenaldo <josenaldo@gmail.com>
2014-05-05 22:27:25 -0300

    Caro Josenaldo do futuro, O FDP do Josenaldo do passado criou uma box muito bugada pra rodar no Windows. Tive que consertar as cagadas que ele fez. Primeiro, tive que ativar a merda dos links simbólicos no windows. Depois, tive que instalar mais um monte de bagaça (olha no diff). E, por fim, vou ter que atualizar o maldito do wiki. Se eu não fizer isso, você, como desmemoriado que é (eu sei, eu sou você) vai esquecer como usa essa box. E o projeto bilionário? Começou? Eu ainda não...

--- Vagrantfile
+++ Vagrantfile
@@ -6,12 +6,17 @@ VAGRANTFILE_API_VERSION = "2"
 
 Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
 
-  config.vm.box = "precise64"
+	config.vm.box = "saucy64"
 
-  config.vm.box_url = "http://files.vagrantup.com/precise64.box"
+	config.vm.box_url = "https://cloud-images.ubuntu.com/vagrant/saucy/20140427/saucy-server-cloudimg-amd64-vagrant-disk1.box"
 
-  config.vm.network :private_network, ip: "192.168.33.10"
+	config.vm.network :private_network, ip: "192.168.33.10"
 
-  config.vm.provision :shell, :path => "bootstrap.sh"
-
+	config.vm.provision :shell, :path => "bootstrap.sh"
+	
+	config.vm.provider :virtualbox do |vb|
+		vb.customize ["modifyvm", :id, "--memory", "1024"]
+		vb.customize ["setextradata", :id, "VBoxInternal2/SharedFoldersEnableSymlinksCreate/vagrant", "1"]
+	end
+	
 end
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,28 +1,48 @@
 #!/usr/bin/env bash
 
+apt-get update
 add-apt-repository ppa:chris-lea/node.js
 apt-get update
 
+apt-get install -y build-essential openssl libreadline6 libreadline6-dev
+apt-get install -y curl
+apt-get install -y libcurl4-gnutls-dev libexpat1-dev gettext libz-dev libssl-dev
+apt-get install -y git
+apt-get install -y ruby
 apt-get install -y apache2
-rm -rf /var/www
-ln -fs /vagrant/app /var/www
-
-export DEBIAN_FRONTEND=noninteractive
-apt-get install -y mysql-server-5.5 mysql-server mysql-client
-
 apt-get install -y php5 libapache2-mod-php5
 apt-get install -y php5-mysql php5-curl php5-gd php5-intl php-pear php5-imagick php5-imap php5-mcrypt php5-memcache 
 apt-get install -y php5-ming php5-ps php5-pspell php5-recode php5-snmp php5-sqlite php5-tidy php5-xmlrpc php5-xsl
 apt-get install -y phpmyadmin
-service apache2 restart
-
 apt-get install -y software-properties-common
 apt-get install -y python-software-properties python g++ make nodejs
+apt-get install -y libjpeg-turbo-progs optipng
+service apache2 restart
 
-rm -rf /usr/bin/node
+export DEBIAN_FRONTEND=noninteractive
+apt-get install -y mysql-server-5.5 mysql-server mysql-client
+
+
+rm -rf /var/www
+ln -fs /vagrant/app /var/www
+
 ln -fs /usr/bin/nodejs /usr/bin/node 
 
+mkdir /tmp/node_modules
+ln -s /tmp/node_modules /vagrant/node_modules
+chmod -R 777 /tmp/node_modules/
+
+gem update --system
+gem install compass
+gem install sass
+
 npm install -g yo
+npm install -g phantomjs
 npm install -g generator-webapp
-npm install -g generator-angular 
+npm install -g generator-angular
+npm install -g http-server
+npm install -g karma
+npm install -g protractor
+npm install -g karma-jasmine 
+npm update -g
 
